a0024f84e18280d47bc55b8ffb950a122869667e
Fabio Muratori <fabio.muratori2@studio.unibo.it>
2023-04-17 16:50:29 +0200

    Attempted (not working) prolog language linting for monaco editor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -127,6 +127,69 @@ val App = FC<Props> {
                             onChange = {
                                 editorTabs.find { it2 -> it2.fileName == editorSelectedTab }?.editorValue = it
                             }
+                            beforeMount = {
+                                console.log("ASD")
+                                console.log(it)
+
+//
+//                                monaco.languages.setMonarchTokensProvider('tuprolog', {
+//
+//                                    symbols: /[=><!~?:&|+\-*\/\^%]+/,
+//
+//                                    escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+//
+//                                    tokenizer: {
+//                                    root: [
+//                                    // functors
+//                                    [/([a-z][a-zA-Z_0-9]*)\s*(?=\()/, 'type.identifier'],
+//
+//                                    // whitespace
+//                                    { include: '@whitespace' },
+//
+//                                    // delimiters and operators
+//                                    [/[{}()\[\]]/, '@brackets'],
+//                                    [/((?!\/\*)[+*\/^<>=~:.?@#$&\\-]+)|!|;|,|rem|mod|is/, 'type.operators'],
+//
+//                                    // numbers
+//                                    [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+//                                    [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+//                                    [/0[oO][0-7]+/, 'number.oct'],
+//                                    [/0[bB][0-1]+/, 'number.bin'],
+//                                    [/\d+/, 'number'],
+//
+//                                    [/[;,.]/, 'delimiter'],
+//
+//                                    // strings
+//                                    [/"([^"\\]|\\.)*$/, 'string.invalid'],  // non-teminated string
+//                                    [/"/, { token: 'string.quote', bracket: '@open', next: '@string' }],
+//
+//                                    // characters
+//                                    [/'[^\\']'/, 'string'],
+//                                    [/(')(@escapes)(')/, ['string', 'string.escape', 'string']],
+//                                    [/'/, 'string.invalid']
+//                                    ],
+//
+//                                    comment: [
+//                                    [/[^\/*]+/, 'comment'],
+//			[/[\/*]/, 'comment'],
+//			[/\/\/.*$/, 'comment'],
+//			[/%.*$/, 'comment']
+//		],
+//
+//		string: [
+//			[/[^\\"]+/, 'string'],
+//			[/@escapes/, 'string.escape'],
+//			[/\\./, 'string.escape.invalid'],
+//			[/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }]
+//		],
+//
+//		whitespace: [
+//			[/[ \t\r\n]+/, 'white'],
+//		]
+//	},
+//})
+
+                            }
                         }
                     }
                 }
